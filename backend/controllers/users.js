9df7445455d180dc43980f6da0742458e2c74a3e
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2023-08-23 22:42:29 +0400

    fix: ПР 15, исправление замечаний ревью 2

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { SECRET } = require('../utils/constants');
 const User = require('../models/user');
 const NotFoundError = require('../errors/NotFoundError');
 const ValidationError = require('../errors/ValidationError');
@@ -30,7 +31,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, SECRET, { expiresIn: '7d' });
       return res.send({ token });
     })
     .catch(next);
--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -6,34 +6,33 @@ class Api {
 
   _checkResponse(res) {
     if (res.ok) {
-      return res.json();
+      // return res.json();
+      return Promise.resolve(res.json());
     } else {
-      return Promise.reject(`код ошибки: ${res.status}`);
+      return Promise.reject(`Код ошибки: ${res.status}`);
     }
   }
 
-  getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
+  async getInitialCards() {
+    const res = await fetch(`${this._baseUrl}/cards`, {
       method: 'GET',
       credentials: 'include',
       headers: this._headers,
-    }).then((res) => {
-      return this._checkResponse(res);
     });
+      return this._checkResponse(res);
   }
 
-  getProfileInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
+  async getProfileInfo() {
+    const res = await fetch(`${this._baseUrl}/users/me`, {
       method: 'GET',
       credentials: 'include',
       headers: this._headers,
-    }).then((res) => {
+  });
       return this._checkResponse(res);
-    });
   }
 
-  editMyProfile({ name, about }) {
-    return fetch(`${this._baseUrl}/users/me`, {
+  async editMyProfile({ name, about }) {
+    const res = await fetch(`${this._baseUrl}/users/me`, {
       method: 'PATCH',
       credentials: 'include',
       headers: this._headers,
@@ -41,13 +40,12 @@ class Api {
         name: name,
         about: about,
       }),
-    }).then((res) => {
-      return this._checkResponse(res);
     });
+    return this._checkResponse(res);
   }
 
-  addPhoto({ name, link }) {
-    return fetch(`${this._baseUrl}/cards`, {
+  async addPhoto({ name, link }) {
+    const res = await fetch(`${this._baseUrl}/cards`, {
       method: 'POST',
       credentials: 'include',
       headers: this._headers,
@@ -56,50 +54,43 @@ class Api {
         link: link,
         likes: [],
       }),
-    }).then((res) => {
-      return this._checkResponse(res);
     });
+      return this._checkResponse(res);
   }
 
-  changeLike(cardId, isLiked) {
-    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+  async changeLike(cardId, isLiked) {
+    const res = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
       method: isLiked ? 'DELETE' : 'PUT',
       credentials: 'include',
       headers: this._headers,
-    }).then((res) => {
-      return this._checkResponse(res);
     });
+      return this._checkResponse(res);
   }
 
-  deleteCard(cardId) {
-    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+  async deleteCard(cardId) {
+    const res = await fetch(`${this._baseUrl}/cards/${cardId}`, {
       method: 'DELETE',
       credentials: 'include',
       headers: this._headers,
-    }).then((res) => {
-      return this._checkResponse(res);
     });
+      return this._checkResponse(res);
   }
 
-  editMyAvatar(data) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
+  async editMyAvatar(data) {
+    const res = await fetch(`${this._baseUrl}/users/me/avatar`, {
       method: 'PATCH',
       credentials: 'include',
       headers: this._headers,
       body: JSON.stringify({ avatar: data.avatar }),
-    }).then((res) => {
-      return this._checkResponse(res);
     });
+      return this._checkResponse(res);
   }
 }
 
 const api = new Api({
-  // baseUrl: 'http://localhost:4000',
-  // baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-65',
   baseUrl: 'https://api.mesto.hope-208.nomoreparties.co',
   headers: {
-    Authorization: `Bearer ${localStorage.getItem('jwt')}`,    
-    // authorization: '107572fd-a23a-435b-9724-668d3d26cd42',
+    Authorization: `Bearer ${localStorage.getItem('jwt')}`,
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
   },
